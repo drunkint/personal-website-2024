f8ee9afad222d484d7b0cd86117db567202b1669
Angus Lee <angus.li2000@gmail.com>
2024-04-20 19:14:45 -0400

    footer and social media

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -11,10 +11,10 @@ const ClickableIcon = props => {
   );
 };
 const IconsData = [
-  { href: "https://github.com/hktitof", Icon: GithubIcon },
-  { href: "https://www.linkedin.com/in/abdellatif-anaflous/", Icon: LinkedinIcon },
-  { href: "https://www.instagram.com/titof_abdo/", Icon: InstagramIcon },
-  { href: "https://www.youtube.com/@abdellatif_anaflous", Icon: YoutubeIcon },
+  { href: "https://github.com/drunkint", Icon: GithubIcon },
+  { href: "https://www.linkedin.com/in/angusleehs/", Icon: LinkedinIcon },
+  { href: "https://www.instagram.com/anguslee__/", Icon: InstagramIcon },
+  // { href: "https://www.youtube.com/@abdellatif_anaflous", Icon: YoutubeIcon },
 ];
 
 export default function Fotter(props: { githubUrl: string; hideSocialsInDesktop: boolean }) {
@@ -32,12 +32,17 @@ export default function Fotter(props: { githubUrl: string; hideSocialsInDesktop:
     text-sm  space-y-2  "
         >
           <span className="group-hover:text-AAsecondary sm:text-sm text-xs">
-            Built by Anaflous Abdellatif
+            Original Code by Anaflous Abdellatif
           </span>
-
-          <span className="text-xs flex flex-row items-center space-x-2 group-hover:text-AAsecondary">
-            <GithubIcon className={"w-4 h-4 text-gray-400 fill-current group-hover:text-AAsecondary"} />
-            <span className="">Source code - Github</span>
+        </div>
+      </a>
+      <a href={'https://github.com/drunkint/cb-website'} className="" target={"_blank"} rel="noreferrer">
+        <div
+          className="group flex flex-col font-mono justify-center items-center  text-gray-400 
+    text-sm  space-y-2  "
+        >
+          <span className="group-hover:text-AAsecondary sm:text-sm text-xs">
+            Modified by Angus Lee (Contributions are welcome!)
           </span>
         </div>
       </a>
