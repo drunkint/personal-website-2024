ad553e5a45b7a8b684ecdf564a56df898a6875f2
Hsu Shen (Angus) Lee <62867164+drunkint@users.noreply.github.com>
2024-07-09 18:58:48 -0400

    attempt fix overflow issue tag

--- components/Blog/post-header.js
+++ components/Blog/post-header.js
@@ -9,32 +9,23 @@ export default function PostHeader({ title, coverImage, date, tags }) {
   return (
     <div className="py-4 mx-auto lg:py-24 lg:mx-auto max-w-4xl">
       <button
-        className="text-lg text-AAsecondary rounded-lg px-4 py-2 hover:bg-AAtertiary "
+        className="text-lg text-AAsecondary rounded-lg pl-4 py-2 hover:bg-AAtertiary "
         type="button"
         onClick={() => router.back()}
       >
         Back
       </button>
-      <h1 className="text-gray-300 font-bold text-7xl px-3 mt-4 py-8">
+      <h1 className="text-gray-300 font-bold text-7xl pl-3 mt-4 py-8">
         {title}
       </h1>
-      <div className="mb-6 text-lg text-gray-300 px-4 mx-auto">
+      <div className="mb-6 text-lg text-gray-300 pl-4 mx-auto">
         <DateFormatter dateString={date} />
-        <div>
-          {/* tags: {
-          <span class="bg-#3B4252 text-AAsecondary text-italic">
-          {tags.join(', ')}
-          </span>
-        
-        } */}
+        <div className="w-full">
           tags:{" "}
           {tags.map((tag) => (
-            // <span class="bg-#3B4252 text-AAsecondary text-italic">
-            //   #{tag}
-            // </span>
             <span
               key={tag}
-              className="bg-AAtertiary text-AAsecondary text-lg font-medium me-2 px-2.5 py-0.5 rounded-full"
+              className=" inline-block bg-AAtertiary text-AAsecondary text-lg font-medium ml-1 px-2.5 py-0.5 rounded-full"
             >
               {tag}
             </span>
